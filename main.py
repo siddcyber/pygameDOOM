301a799c036e3fabc65e6c51b0142e7d807f325b
siddcyber <siddhantgulia@gmail.com>
2022-09-18 00:51:38 +0530

    Bug: wall collision not working, passed through, known wall checking bug fixed: changed map rectangle position/size to accomodate the mainmap bug fixed update: changed map manually

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from player import *
 class DoomGameMain:
     def __init__(self):
         pg.init()
-        self.screen = pg.display.set_mode(resolution)
+        self.screen = pg.display.set_mode((1600,900))
         self.clock = pg.time.Clock()
         self.delta_time = 1
         self.newGame()
