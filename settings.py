a144316f80e3fd1547aecfb2f246ba792dec734b
siddcyber <siddhantgulia@gmail.com>
2022-09-18 19:58:48 +0530

    added resolution generalization code

--- settings.py
+++ settings.py
@@ -2,15 +2,20 @@ import math
 from win32api import GetSystemMetrics
 
 # game settings
-width = int(GetSystemMetrics(0))
-height = int(GetSystemMetrics(1))
-halfWidth = width // 2
-halfHeight = height // 2
-resolution = width, height
-# resolution = halfWidth, halfHeight
-FPS = 60
+# windowSize = "1080p"
+windowSize = "720p"
 
-playerPosition = 1.5, 5  # mini_map
+if windowSize == "1080p":
+    width = int(GetSystemMetrics(0))
+    height = int(GetSystemMetrics(1))
+elif windowSize == "720p":
+    width = int(GetSystemMetrics(0)) // 2
+    height = int(GetSystemMetrics(1)) // 2
+
+resolution = width, height
+
+FPS = 60
+playerPosition = 1, 1  # mini_map
 playerAngle = 0
 playerSpeed = 0.004
 # playerSpeed = 0.004
